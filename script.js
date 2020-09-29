6270e6da30e121a0e3ae317ec0d3428563edce84
Shubham Kumar <rajputshubhamshubhamrajput019@gmal.com>
2020-09-29 09:37:07 +0530

    HTML And Instruction Script setup

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+--> Guess the Number Game
+* TODO: Get User Value From input and save it to variable number
+* TODO: Generate a random number 1 to 100 and save it to a variable
+* TODO: Console whether the guess is too high, too low, or is correct
+* TODO: Create a fucntion called displayResult to move the
+* TODO: Complete the showYouWon, showNumberAbove, showNumberBelow
+* TODO: Use the showYouWon... functions within displayResult to
+* TODO: Save the guess history in a variable called guess.
+* TODO: Display the guess history using displayHistory() function
+*TODO: Use the initGame() function to restart the game
+
+*/
